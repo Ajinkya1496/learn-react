7e73b5df036d276103c3f9df04672fe69292a799
apple <apple@apples-MacBook-Pro.local>
2020-06-17 14:33:18 +0530

    components in sync

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/navbar';
+import Counters from './components/counters';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = { 
+    counters: [
+        {id: 1, value: 4},
+        {id: 2, value: 2},
+        {id: 3, value: 1},
+        {id: 4, value: 0},
+    ]
+ }
+
+handleIcrement = counter => {
+    let counters = [...this.state.counters];
+    const index = this.state.counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value ++;
+    this.setState({counters});
+}
+handleDelete = index => {
+    let counters = this.state.counters.filter( counter => counter.id !== index);
+    this.setState({counters});
+}
+handleReset = () => {
+    let counters = this.state.counters.map(counter => {
+        counter.value = 0;
+        return counter;
+    })
+
+    this.setState({counters});
+}
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar totalCount={this.state.counters.filter(counter => counter.value > 0).length}/>
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset = {this.handleReset}
+            onIncrement = {this.handleIcrement}
+            onDelete = {this.handleDelete}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
